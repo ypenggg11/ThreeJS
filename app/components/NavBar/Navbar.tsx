d7725397e9cbdc52180045da2f8b4c3e53901b71
Peng You <ypenggg11@gmail.com>
2023-06-08 22:01:09 +0100

    navbar done

--- app/components/NavBar/Navbar.tsx
+++ app/components/NavBar/Navbar.tsx
@@ -6,12 +6,12 @@ import Image from 'next/image'
 import { useState } from 'react'
 import styles from './NavBar.module.scss'
 import spacingStyles from '@/styles/global-spacing.module.scss'
-import { logo } from '@/public/assets'
+import { close, logo, menu } from '@/public/assets'
 import { navLinks } from '@/constants/data'
 
 export default function Navbar() {
   const [active, setActive] = useState<string>()
-  console.log(active)
+  const [toggle, setToggle] = useState(false)
 
   return (
     <nav className={`${spacingStyles['padding-x']} ${styles['nav-container']}`}>
@@ -29,19 +29,62 @@ export default function Navbar() {
             alt='logo'
             width={36}
             height={36}
-            style={{ objectFit: 'contain' }}
+            className={styles.image}
           />
           <p className={styles.author}>
-            Peng <span>| Portfolio</span>
+            Peng &nbsp; <span className={styles.block}>| Portfolio</span>
           </p>
         </Link>
         <ul className={styles.list}>
           {navLinks.map((link) => (
-            <li key={link.id}>
-              <Link href={`#${link.id}`}>{link.title}</Link>
+            <li
+              key={link.id}
+              className={`${styles['list-item']} ${
+                active === link.title ? styles.active : ''
+              }`}
+            >
+              <Link href={`#${link.id}`} onClick={() => setActive(link.title)}>
+                {link.title}
+              </Link>
             </li>
           ))}
         </ul>
+        <div className={styles.menu}>
+          <Image
+            src={toggle ? close : menu}
+            alt='menu'
+            width={28}
+            height={28}
+            className={styles.image}
+            onClick={() => setToggle((prevState) => !prevState)}
+          />
+          <div
+            className={`black-gradient ${styles['menu-modal']} ${
+              !toggle ? styles.hide : ''
+            }`}
+          >
+            <ul className={styles['menu-list']}>
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${styles['list-item']} ${
+                    active === link.title ? styles.active : ''
+                  }`}
+                >
+                  <Link
+                    href={`#${link.id}`}
+                    onClick={() => {
+                      setActive(link.title)
+                      setToggle((prevState) => !prevState)
+                    }}
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
